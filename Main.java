a6d92087cad22a6fdf72520019ade0e04f792f42
elqady <elqady.hesham@gmail.com>
2023-03-22 09:11:43 +0100

    primo programma

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Dammi la parola");
+        String word = in.nextLine();
+        System.out.println("dammi il numero di anagrams che vuoi");
+        int numAnagrams = in.nextInt();
+        ArrayList<String> anagrams = new ArrayList<>();
+        anagrams = printPermutn(word, "");
+        for (int i = 1; i <= numAnagrams; i++) {
+            System.out.println(anagrams.get(i));
+        }
+
+    }
+
+    public static String[] printNPermutations(String str, String ans, int n) {
+
+        String[] anagrams = new String[n];
+        for (int k =0; k<=n; k++){
+
+        }
+        if (str.length() == 0) {
+            //System.out.print(ans + " ");
+            return anagrams;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            // ith character of str
+            char ch = str.charAt(i);
+
+            // Rest of the string after excluding
+            // the ith character
+            String ros = str.substring(0, i) +
+                    str.substring(i + 1);
+
+            // Recursive call
+            printPermutn(ros, ans + ch);
+        }
+        return anagrams;
+    }
+
+
+}
+
+
+
+
