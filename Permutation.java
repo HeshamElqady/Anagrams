cb00e2f811a28139d63d0a821832cd0b3d06a450
elqady <elqady.hesham@gmail.com>
2023-03-22 15:56:41 +0100

    tutte le permutazioni di una parola w

--- Permutation.java
+++ Permutation.java
@@ -1,43 +1,21 @@
-/*public static void main(String[] args) {
+import java.util.Scanner;
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Dammi la parola");
-        String word=in.nextLine();
-        System.out.println("dammi il numero di anagrams che vuoi");
-        int numAnagrams = in.nextInt();
-        ArrayList<String> anagrams = new ArrayList<>();
-        anagrams=printPermutn(word,"");
-        for (int i=1; i<=numAnagrams;i++){
-        System.out.println(anagrams.get(i));
+public class Permutation {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word to generate permutations: ");
+        String word = scanner.nextLine();
+        permutation("", word);
+
+    }
+
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
         }
-
-        }
-
-public static ArrayList<String> printPermutn(String str, String ans)
-        {
-
-        ArrayList<String> anagrams = new ArrayList<>();
-        if (str.length() == 0) {
-        //System.out.print(ans + " ");
-        anagrams.add(ans);
-        return anagrams ;
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-
-        // ith character of str
-        char ch = str.charAt(i);
-
-        // Rest of the string after excluding
-        // the ith character
-        String ros = str.substring(0, i) +
-        str.substring(i + 1);
-
-        // Recursive call
-        printPermutn(ros, ans + ch);
-        }
-        return anagrams;
-        }
-
-
-        }*/
+    }
+}
